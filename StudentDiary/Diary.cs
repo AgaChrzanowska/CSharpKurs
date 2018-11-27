ab46bc0826dba838a6fbc00172626d0dd713d32c
diwadu <diwadu@gmail.com>
2018-11-27 18:52:11 +0100

    local changes - no idea wtf is this xD

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // Stan - zmienne/pola
+        List<float> ratings = new List<float>();
+
+        // Zachowania - metody, które chcemy wpisać, aby umożliwić współdziałania z dzienniczkiem.
+        public void AddRaiting(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();
+            return avg;
+        }
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+n(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRaiting(5);
+            diary.AddRaiting(8.5f);
+            diary.AddRaiting(4.7f);
+
+            float avg = diary.CalculateAverage();
+            float max = diary.GiveMaxRating();
+            float min = diary.GiveMinRating();
+
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocenę z zakresu 1-10");
+
+                float rating = float.Parse(Console.ReadLine());
+
+                diary.AddRaiting(rating);
+            }
+        }
+    }
+}
